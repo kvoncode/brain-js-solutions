9052071d548decf86287353d428bb6d0569804fb
winepkd <winepkd@gmail.com>
2019-05-01 15:07:49 +0900

    add: xor net with logging error

--- scrimba-tutorial/1. introduction/index.pack.js
+++ scrimba-tutorial/1. introduction/index.pack.js
@@ -3,15 +3,22 @@
 // input 1 0, output 1
 // input 1 1, output 0
 
+// eslint-disable-next-line no-undef
 const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
 
-const tainingData = [
+const trainingData = [
     { input: [0, 0], output: [0] },
     { input: [0, 1], output: [1] },
     { input: [1, 0], output: [1] },
     { input: [1, 1], output: [0] }
 ]
 
-net.train(trainingData);
+net.train(trainingData, {
+    log: (error) => console.log(error),
+    logPeriod: 1000
+});
 
 console.log(net.run([0, 0]))
+console.log(net.run([0, 1]))
+console.log(net.run([1, 0]))
+console.log(net.run([1, 1]))
